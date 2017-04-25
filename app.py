2e4e4b452b70648f67974aaa5579cd009be6a796
GreywindGit <GreywindGit@users.noreply.github.com>
2017-04-25 22:42:24 +0200

    Add delete and modify functions

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
-from file_handler import load_stories, save_stories
+from file_handler import load_stories, save_story, delete_story, load_story, modify_story
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/list', methods=['GET', 'POST'])
 def list_stories():
+    delete_id = request.args.get('delete', '')
+    if delete_id != '':
+        delete_story(delete_id, 'stories.csv')
     if request.method == 'POST':
         new_entry = list()
-        new_entries = list()
         fields = ['story_title', 'user_story', 'acc_criteria', 'business_value', 'estimation', 'status']
         for field in fields:
             new_entry.append(request.form[field])
-        new_entries.append(new_entry)
-        save_stories(new_entries, 'stories.csv')
+        if request.form['story_id'] == '':
+            save_story(new_entry, 'stories.csv')
+        else:
+            modify_story(request.form['story_id'], new_entry, 'stories.csv')
     stories = load_stories('stories.csv')
     table_header = ['ID', 'Story Title', 'User Story', 'Acceptance criteria', 'Business Value', 'Estimation', 'Status']
     return render_template('list.html', stories=stories, table_header=table_header)
@@ -23,9 +27,9 @@ def list_stories():
 @app.route('/story/<story_id>')
 def new_story(story_id=None):
     if story_id:
-        edited_entry = load_stories('stories.csv')[int(story_id)-1]
+        edited_entry = load_story(story_id, 'stories.csv')
         return render_template('story.html', story_id=story_id, editing=edited_entry)
-    return render_template('story.html', story_id=story_id)
+    return render_template('story.html', story_id=story_id, editing=['' * 7])
 
 
 if __name__ == '__main__':
--- file_handler.py
+++ file_handler.py
@@ -1,4 +1,4 @@
-def load_stories(filename):
+def load_stories(filename="stories.csv"):
     """ Loads data from specified file. Returns a list of entries with
         their specific field values.
     """
@@ -7,6 +7,7 @@ def load_stories(filename):
     try:
         with open(filename, 'r') as workfile:
             for line in workfile:
+                line = line.strip('\n')
                 stories.append(list(str(id)) + line.split('#'))
                 id += 1
     except FileNotFoundError:
@@ -14,12 +15,59 @@ def load_stories(filename):
     return stories
 
 
-def save_stories(new_entries, filename="stories.csv"):
+def save_story(new_entry, filename="stories.csv"):
     try:
         with open(filename, 'a') as workfile:
-            for new_entry in new_entries:
-                workfile.write('#'.join(new_entry) + '\n')
+            workfile.write('#'.join(new_entry) + '\n')
     except FileNotFoundError:
         with open(filename, 'w') as workfile:
-            for new_entry in new_entries:
-                workfile.write('#'.join(new_entry) + '\n')
+            workfile.write('#'.join(new_entry) + '\n')
+
+
+def delete_story(delete_id, filename="stories.csv"):
+    stories = list()
+    id = 1
+    try:
+        with open(filename, 'r') as workfile:
+            for line in workfile:
+                if id != int(delete_id):
+                    stories.append(line)
+                id += 1
+    except FileNotFoundError:
+        stories = None
+    else:
+        with open(filename, 'w') as workfile:
+                for story in stories:
+                    workfile.write(story)
+
+
+def load_story(story_id, filename="stories.csv"):
+    id = 1
+    try:
+        with open(filename, 'r') as workfile:
+            for line in workfile:
+                if id == int(story_id):
+                    line = line.strip('\n')
+                    return (list(str(id)) + line.split('#'))
+                id += 1
+    except FileNotFoundError:
+        return
+
+
+def modify_story(story_id, new_entry, filename="stories.csv"):
+    id = 1
+    stories = list()
+    try:
+        with open(filename, 'r') as workfile:
+            for line in workfile:
+                if id == int(story_id):
+                    stories.append('#'.join(new_entry) + '\n')
+                else:
+                    stories.append(line)
+                id += 1
+    except FileNotFoundError:
+        pass
+    else:
+        with open(filename, 'w') as workfile:
+            for story in stories:
+                workfile.write(story)
