a72c4a9317d2e0a915712ed2a761656054570b2e
GreywindGit <GreywindGit@users.noreply.github.com>
2017-04-25 10:18:50 +0200

    Add "create new story"

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-from file_handler import load_stories
+from flask import Flask, render_template, request
+from file_handler import load_stories, save_stories
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/list')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/list', methods=['GET', 'POST'])
 def lists():
+    if request.method == 'POST':
+        new_entry = []
+        fields = ['story_title', 'user_story', 'acc_criteria', 'business_value', 'estimation', 'status']
+        for field in fields:
+            new_entry.append(request.form[field])
+        save_stories(new_entry, 'stories.csv')
     stories = load_stories('stories.csv')
     table_header = ['ID', 'Story Title', 'User Story', 'Acceptance criteria', 'Business Value', 'Estimation', 'Status']
     return render_template('list.html', stories=stories, table_header=table_header)
--- file_handler.py
+++ file_handler.py
@@ -1,11 +1,23 @@
 def load_stories(filename):
+    """ Loads data from specified file. Returns a list of entries with
+        their specific field values.
+    """
     stories = list()
     id = 1
     try:
         with open(filename, 'r') as workfile:
             for line in workfile:
-                stories.append(list(str(id)) + line.split(','))
+                stories.append(list(str(id)) + line.split('#'))
                 id += 1
     except FileNotFoundError:
         stories = None
     return stories
+
+
+def save_stories(new_entry, filename="stories.csv"):
+    try:
+        with open(filename, 'a') as workfile:
+            workfile.write('#'.join(new_entry) + '\n')
+    except FileNotFoundError:
+        with open(filename, 'w') as workfile:
+            workfile.write('#'.join(new_entry) + '\n')
